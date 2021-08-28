3680849d42baf0f183d2197b3159edbf0cc00c3a
Christopher O'Brien <irishsoxmax@gmail.com>
2021-08-28 13:39:41 -0700

    updates to testing & code structure

--- Program.cs
+++ Program.cs
@@ -10,29 +10,40 @@ namespace FantasyGroundsPackager
     {
         static void Main(string[] args)
         {
-            string rootDir = @"C:\Users\User\source\repos\FantasyGroundsPackager\FG-PFRPG-Live-Hitpoints";
+            string rootDir = args[0];
+            //string rootDir = @"C:\Users\User\source\repos\FantasyGroundsPackager\FG-PFRPG-Live-Hitpoints";
             FileAttributes rootDirAttributes = File.GetAttributes(rootDir);
             rootDirAttributes.HasFlag(FileAttributes.Directory);
-            XmlDocument extensionFile = new XmlDocument();
-            List<string> validFiles = new List<string> { "extension.xml" };
-            extensionFile.Load($@"{rootDir}\extension.xml");
-            XmlNodeList includes = extensionFile.SelectNodes("/root/base/*");
-            foreach (XmlNode fileNode in includes)
+            List<string> validFiles = ReadPackageContents(Path.Join(rootDir, "extension.xml"));
+
+            string packageFile = Path.Join(rootDir, $"{Path.GetFileName(rootDir)}.ext");
+            if(File.Exists(packageFile))
             {
-                if(fileNode.Name == "includefile") {
+                File.Delete(packageFile);
+            }
+            using ZipArchive archive = ZipFile.Open(packageFile, ZipArchiveMode.Create);
+            foreach (string file in validFiles)
+            {
+                archive.CreateEntryFromFile(Path.Join(rootDir, file), file);
+            }
+        }
+        static List<string> ReadPackageContents(string extensionFile)
+        {
+            List<string> validFiles = new() { "extension.xml" };
+            XmlDocument extensionDoc = new();
+            extensionDoc.Load(extensionFile);
+            foreach (XmlNode fileNode in extensionDoc.SelectNodes("/root/base/*"))
+            {
+                if (fileNode.Name == "includefile")
+                {
                     validFiles.Add(fileNode.Attributes.GetNamedItem("source").Value);
-                } else if(fileNode.Name == "script" || fileNode.Name == "icon") {
+                }
+                else if (fileNode.Name == "script" || fileNode.Name == "icon")
+                {
                     validFiles.Add(fileNode.Attributes.GetNamedItem("file").Value);
                 }
             }
-            
-            using (ZipArchive archive = ZipFile.Open($@"{rootDir}\release.zip", ZipArchiveMode.Update))
-            {
-                foreach (string file in validFiles)
-                {
-                    archive.CreateEntryFromFile($@"{rootDir}\{file}", file);
-                }
-            }
+            return validFiles;
         }
     }
 }
